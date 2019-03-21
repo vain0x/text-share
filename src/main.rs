ca8e9666f847c640d8d448d0e9eea1072c76ddfd
vain0x <vainzerox@gmail.com>
2019-03-22 00:45:43 +0900

    feat: Update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,87 @@
-
 //! Example actix-web application.
 //!
 //! This code is adapted from the front page of the [Actix][] website.
 //!
 //! [actix]: https://actix.rs/docs/
 
-use actix_web::{server, App, HttpRequest, Responder};
-use std::env;
+#[macro_use]
+extern crate serde_derive;
 
-fn greet(req: &HttpRequest) -> impl Responder {
-    let to = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", to)
+use actix_web::{
+    http::Method, server, App, AsyncResponder, Form, FromRequest, HttpMessage, HttpRequest,
+    HttpResponse,
+};
+use futures::future::Future;
+use std::collections::BTreeMap;
+use std::env;
+use std::sync::{Arc, Mutex};
+
+struct State {
+    map: BTreeMap<String, String>,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            map: BTreeMap::new(),
+        }
+    }
+
+    fn get(&self, key: &str) -> Option<String> {
+        self.map.get(key).cloned()
+    }
+
+    fn add(&mut self, key: String, value: String) {
+        if value.len() >= 10000 {
+            return;
+        }
+
+        if self.map.len() >= 1000 {
+            self.map.clear();
+        }
+
+        self.map.insert(key, value);
+    }
+}
+
+type AsyncState = Arc<Mutex<State>>;
+
+#[derive(Deserialize)]
+struct IndexPostArgs {
+    key: String,
+    value: String,
+}
+
+fn get_index(_req: HttpRequest<AsyncState>) -> HttpResponse {
+    HttpResponse::Ok().body(include_str!("index.html"))
+}
+
+fn post_index(
+    req: HttpRequest<AsyncState>,
+) -> Box<Future<Item = HttpResponse, Error = actix_web::Error>> {
+    let args = Form::<IndexPostArgs>::extract(&req);
+    let state = req.state().clone();
+
+    let response = get_index(req);
+
+    args.map(move |args| {
+        let args = args.into_inner();
+        state.lock().unwrap().add(args.key, args.value);
+        response
+    })
+    .responder()
+}
+
+fn get_key(req: HttpRequest<AsyncState>) -> HttpResponse {
+    let key = req.match_info().get("key").unwrap_or("");
+    let value = match req.state().lock().unwrap().get(key) {
+        Some(value) => value.clone(),
+        None => return HttpResponse::NotFound().finish(),
+    };
+
+    HttpResponse::Ok()
+        .header("X-Content-Type-Options", "no-sniff")
+        .body(value)
 }
 
 fn main() {
@@ -22,9 +93,11 @@ fn main() {
 
     // Start a server, configuring the resources to serve.
     server::new(|| {
-        App::new()
-            .resource("/", |r| r.f(greet))
-            .resource("/{name}", |r| r.f(greet))
+        App::with_state(Arc::new(Mutex::new(State::new())))
+            .route("/", Method::GET, get_index)
+            .route("/", Method::POST, post_index)
+            .route("/{key}", Method::GET, get_key)
+            .finish()
     })
     .bind(("0.0.0.0", port))
     .expect("Can not bind to port 8000")
